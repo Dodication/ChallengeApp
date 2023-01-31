78318613246c3da3e9230c2e7ee33f6008f387b5
teterycz <teter@DESKTOP-0DQ2FO3>
2023-01-31 20:16:24 +0100

    lekcja 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Dorota";
+string plec = "Kobieta";
+int wiek = 31;
+if (plec == "Kobieta" && wiek < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("kobieta powyżej 30 lat");
+}

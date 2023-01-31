9d03264a1c38539159d0fb39c7428b71bcc195f4
teterycz <teter@DESKTOP-0DQ2FO3>
2023-01-31 20:56:41 +0100

    lekcja 4 poprawiona

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,11 +1,14 @@
-﻿string name = "Dorota";
-string sex = "Kobieta";
-int age = 31;
-if (sex == "Kobieta" && age < 30)
+﻿string name = "Ewa";
+    string sex = "Female";
+    int age = 33;
+if (name == "Ewa" && age == 33){
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex != "Female" && age == 18)
+{
+    Console.WriteLine("niepełnoletni mężczyna");
+}
+if (age < 30 && sex == "Female")
 {
     Console.WriteLine("kobieta poniżej 30 lat");
-}
-else
-{
-    Console.WriteLine("kobieta powyżej 30 lat");
 }
